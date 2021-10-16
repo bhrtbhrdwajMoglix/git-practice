71f92984e309df4f841a6f9a802775612abc6189
bhrtbhrdwajMoglix <bharat.bhardwaj@moglix.com>
2021-10-16 10:52:17 +0530

    8 added

--- src/main/java/com/example/mongo/DtaToStringJson1Application.java
+++ src/main/java/com/example/mongo/DtaToStringJson1Application.java
@@ -21,7 +21,7 @@ public class DtaToStringJson1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(DtaToStringJson1Application.class, args);
 		LOGGER.debug("Average finder v0.1");
-		double avg = findAverage(new String[]{"1", "2", "3", "4","5", "6", "7"});
+		double avg = findAverage(new String[]{"1", "2", "3", "4","5", "6", "7", "8"});
 		LOGGER.info("Average is {}.", avg);
 		LOGGER.info("conflict");
 
